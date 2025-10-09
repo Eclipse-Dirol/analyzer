e172ccddddd1b2ee63bcc6e614b468166f4526c0
Gleb Bikbaev <bikbaevgleb4@gmail.com>
2025-10-09 20:48:15 +0500

    bug fix

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from cleaner import cleaners
 import пробник
 import panel
 import read_file
+import methods
 def main():
     path = input('Введите путь к файлу:\n')
     filepath = read_file.filepath(path)
@@ -12,8 +13,8 @@ def main():
     words = c.dict_1(parts)
     template = c.template(words)
     last_template = c.last_template(template)
-    word_count = utils.word_count(last_template)
-    length_word = utils.length_words(last_template)
+    word_count = methods.word_count(last_template)
+    length_word = methods.length_words(last_template)
     panel.show_panel(word_count, length_word)
 if __name__ == "__main__":
     main()
